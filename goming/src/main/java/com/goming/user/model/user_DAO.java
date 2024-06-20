82e451483596c5fb20733e6fbcd8a2449cd8a0e6
kimjaehui02 <momg4287@naver.com>
2024-06-20 10:12:39 +0900

    날씨와 로그인을 결합 성공함

--- goming/src/main/java/com/goming/user/model/user_DAO.java
+++ goming/src/main/java/com/goming/user/model/user_DAO.java
@@ -5,14 +5,25 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import com.goming.user.database.SqlSessionManager;
 import com.goming.user.model.user_DTO;
 
-public class user_DAO {
+public class user_DAO
+{
 
-    SqlSessionFactory factory = SqlSessionManager.getsqlSessionFactory();
+	// 0. Factory 가지고 오기
+	SqlSessionFactory factory = SqlSessionManager.getsqlSessionFactory();
+	SqlSession session;
 
-    public int join(user_DTO dto) {
-        SqlSession session = factory.openSession(true);
-        int row = session.insert("com.goming.user.database.user_mapper.join", dto);  // ensure "join" is used
-        session.close();
-        return row;
-    }
+	public int join(user_DTO dto)
+	{
+		SqlSession session = factory.openSession(true);
+		int row = session.insert("com.goming.user.database.user_mapper.join", dto); // ensure "join" is used
+		session.close();
+		return row;
+	}
+//	public int join(user_DTO dto)
+//	{
+//		SqlSession session = factory.openSession(true);
+//		int row = session.insert("com.goming.user.database.user_mapper.join", dto); // ensure "join" is used
+//		session.close();
+//		return row;
+//	}
 }
