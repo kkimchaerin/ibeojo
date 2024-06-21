d2643aac3696b3a0817867dce02597f40693a540
신현주 <31871923+shju0317@users.noreply.github.com>
2024-06-21 15:02:19 +0900

    기존 작업물

--- goming/src/main/java/com/goming/user/model/user_DAO.java
+++ goming/src/main/java/com/goming/user/model/user_DAO.java
@@ -5,76 +5,104 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import com.goming.user.database.SqlSessionManager;
 import com.goming.user.model.user_DTO;
 
-public class user_DAO
-{
+public class user_DAO {
 
 	// 0. Factory 가지고 오기
 	SqlSessionFactory factory = SqlSessionManager.getsqlSessionFactory();
 	SqlSession session;
 
-	public int join(user_DTO dto)
-	{
+	// 회원가입
+	public int join(user_DTO dto) {
 		session = factory.openSession();
 		int cnt = 0;
 
 		System.out.println("user_DAO : " + "날씨 업소트 진입");
-		try
-		{
+		try {
 
 			System.out.println("user_DAO : " + "try문 진입");
 			// 첫 번째로 날짜와 시간, 좌표값이 모두 일치하는 데이터가 있는지 검사
 			int count = session.selectOne("com.goming.user.database.user_mapper.selectuser_email", dto);
 
-	        Integer emailCount = session.selectOne("com.goming.user.database.user_mapper.selectuser_email", dto);
-	        if (emailCount != 0) {
-	            count += emailCount;
-	            cnt = -1;
-	        }
-	        System.out.println("user_DAO : " + "이메일 체크 : " + emailCount);
-	        System.out.println("user_DAO : " + "이메일 체크 : " + cnt);
+			Integer emailCount = session.selectOne("com.goming.user.database.user_mapper.selectuser_email", dto);
+			if (emailCount != null) {
+				count += emailCount;
+				cnt = -1;
+			}
+			System.out.println("user_DAO : " + "이메일 체크 : " + emailCount);
 
-	        Integer nickCount = session.selectOne("com.goming.user.database.user_mapper.selectuser_nick", dto);
-	        if (nickCount != 0) {
-	            count += nickCount;
-	            cnt = -2;
-	        }
-	        System.out.println("user_DAO : " + "닉네임 체크 : " + nickCount);
-	        System.out.println("user_DAO : " + "닉네임 체크 : " + cnt);
-	        
-			if (count == 0)
-			{
+			Integer nickCount = session.selectOne("com.goming.user.database.user_mapper.selectuser_nick", dto);
+			if (nickCount != null) {
+				count += nickCount;
+				cnt = -2;
+			}
+			System.out.println("user_DAO : " + "이메일 체크" + nickCount);
+
+			if (count == 0) {
 				System.out.println("user_DAO : " + "중복값이 존재하지않습니다");
 				// 검색 결과가 있으면 기존 데이터 업데이트
 				cnt = session.insert("com.goming.user.database.user_mapper.join", dto);
-			}
-			else
-			{
-				
+			} else {
+
 			}
 
-			if (cnt > 0)
-			{
+			if (cnt > 0) {
 				session.commit();
-			} else
-			{
+			} else {
 				session.rollback();
 			}
-		} catch (Exception e)
-		{
+		} catch (Exception e) {
 			System.out.println("user_DAO : " + "가입 실패...");
 			e.printStackTrace();
-		} finally
-		{
+		} finally {
 			session.close();
 		}
 
 		return cnt;
 	}
-//	public int join(user_DTO dto)
-//	{
-//		SqlSession session = factory.openSession(true);
-//		int row = session.insert("com.goming.user.database.user_mapper.join", dto); // ensure "join" is used
-//		session.close();
-//		return row;
-//	}
+
+	// 로그인
+	public user_DTO login(user_DTO dto) {
+
+		// 1. factory 내부의 Session 객체 열어주기
+		SqlSession session = factory.openSession(true);
+
+		// sql 연결
+		user_DTO m = session.selectOne("com.goming.user.database.user_mapper.login", dto);
+
+		// session 자원 반납
+		session.close();
+		// controller에 결과값 보내주기
+		return m;
+
+	}
+
+	// 아이디 찾기
+	public String findIdByNick(String nick) {
+		SqlSession session = factory.openSession(true);
+		String foundId = null;
+
+		try {
+			foundId = session.selectOne("findIdByNick", nick);
+		} finally {
+			session.close();
+		}
+
+		return foundId;
+	}
+
+	public boolean updatePasswordByEmail(String user_email, String user_pw) {
+		SqlSession session = factory.openSession(true);
+		boolean isUpdated = false;
+
+		try {
+			int result = session.update("updatePasswordByEmail", new user_DTO(user_email, user_pw));
+			if (result > 0) {
+				isUpdated = true;
+			}
+		} finally {
+			session.close();
+		}
+
+		return isUpdated;
+	}
 }
