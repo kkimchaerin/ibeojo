8a19c85e0b78c7a073b956a5e7081a2dd704c181
kimjaehui02 <momg4287@naver.com>
2024-06-26 18:05:09 +0900

    카테고리 분류

--- goming/src/main/java/com/goming/mypage/controller/ImgController.java
+++ goming/src/main/java/com/goming/mypage/controller/ImgController.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import com.goming.mypage.model.MyPageDAO;
 import com.goming.mypage.model.MyPageDTO;
+import com.goming.post.model.PostDTO;
 
 public class ImgController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -30,13 +31,17 @@ public class ImgController extends HttpServlet {
 		MyPageDTO dto_img = new MyPageDTO();
 		dto_img.setUser_email(email);
 
-		List<MyPageDTO> img_list = dao.SelectMyPageImg(dto_img);
+		List<PostDTO> img_list = dao.SelectMyPageImg2(dto_img);
 
 		if (img_list != null) {
 			// 불러오기 성공하면 세션에 저장 후 마이페이지로 이동
 			System.out.println("성공한거");
 			session.setAttribute("img_list", img_list);
 			System.out.println(img_list.size());
+			System.out.println(img_list.get(0));
+			System.out.println(img_list.get(0).getSeason());
+			System.out.println(img_list.get(0).getStyle_tag());
+			System.out.println(img_list.get(0).getPost_img());
 			response.sendRedirect("MyPage.jsp");
 		} else {
 			// 불러오기 실패하면 이전 페이지로 이동
